7ce702c080b3ac1e9d5006b7b8b4f7bbae5b835c
TEJ PRATAP <tejfaster55@gmail.com>
2021-02-13 23:33:45 +0530

    txteditor

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,70 +1,66 @@
-import React, { Component } from "react";
-import { Editor } from "react-draft-wysiwyg";
-import { EditorState, convertToRaw } from "draft-js";
+// export default class TextEditor extends Component {
+//   state = {
+//     editorState: EditorState.createEmpty(),
+//   };
 
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import draftToHtml from "draftjs-to-html";
+//   onEditorStateChange = (editorState) => {
+//     this.setState({
+//       editorState,
+//     });
+//   };
 
-export default class TextEditor extends Component {
-  state = {
-    editorState: EditorState.createEmpty(),
-  };
-
-  onEditorStateChange = (editorState) => {
-    this.setState({
-      editorState,
-    });
-  };
-
-  render() {
-    const { editorState } = this.state;
-    //console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())));
-    return (
-      <div>
-        <Editor
-          editorState={editorState}
-          toolbarClassName="toolbarClassName"
-          wrapperClassName="wrapperClassName"
-          editorClassName="editorClassName"
-          onEditorStateChange={this.onEditorStateChange}
-        />
-        {/* <textarea
-          disabled
-          value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
-        ></textarea> */}
-      </div>
-    );
-  }
-}
-
-
-// import React, { useState } from 'react'
-// import { Editor } from 'react-draft-wysiwyg';
-// import { EditorState } from "draft-js"
-// import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-
-// const TextEditor = () => {
-//     const [editorState, setEditorState] = useState(
-//         EditorState.createEmpty()
-//     )
-
-//     function onEditorStateChange(editorState) {
-//         setEditorState({
-//             editorState,
-//         })
-//     }
+//   render() {
+//     const { editorState } = this.state;
+//     //console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())));
 //     return (
-//         <div>
-//             <Editor
-//                 editorState={editorState}
-//                 toolbarClassName="toolbarClassName"
-//                 wrapperClassName="wrapperClassName"
-//                 editorClassName="editorClassName"
-//                 onEditorStateChange={onEditorStateChange}
-//             />
-
-//         </div>
-//     )
+//       <div>
+//         <Editor
+//           editorState={editorState}
+//           toolbarClassName="toolbarClassName"
+//           wrapperClassName="wrapperClassName"
+//           editorClassName="editorClassName"
+//           onEditorStateChange={this.onEditorStateChange}
+//         />
+//         <textarea
+//           //enabled
+//           disabled
+//           value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+//         ></textarea>
+//       </div>
+//     );
+//   }
 // }
 
-// export default TextEditor
+
+import React, { useState } from 'react'
+import { Editor } from 'react-draft-wysiwyg';
+import { EditorState, convertToRaw } from "draft-js";
+
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import draftToHtml from "draftjs-to-html";
+
+const TextEditor = () => {
+    const [editorState, setEditorState] = useState(
+        EditorState.createEmpty()
+    )
+
+  
+    return (
+        <div>
+            <Editor
+                editorState={editorState}
+                toolbarClassName="toolbarClassName"
+                wrapperClassName="wrapperClassName"
+                editorClassName="editorClassName"
+                onEditorStateChange={setEditorState}
+            />
+            {/* <textarea
+                //enabled
+                disabled
+                value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+            ></textarea> */}
+        </div>
+    )
+}
+
+export default TextEditor
